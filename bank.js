4361ed702d0121b9b7b6fd9fc616d6bfa78754d9
hussain-shiyas <hussainshiyas2552@gmail.com>
2022-10-08 09:41:47 +0530

    secnd

--- bank.js
+++ bank.js
@@ -14,6 +14,30 @@ else
 {
     localStorage.setItem(acnoDetails.acno,JSON.stringify(acnoDetails));
     alert('register sucessfully');
-    // window.location.href="index.html";
+        window.location.href="index.html";
 }
+}
+
+
+
+function login(){
+    acno=login_acno.value;
+    pswd=login_pswd.value;
+
+    if(acno in localStorage)
+    {
+        acnoDetails=JSON.parse(localStorage.getItem(acno));
+
+
+        if(pswd==acnoDetails.pswd){
+            window.location.href="index.html"
+        }
+        else{
+            alert('invalid password')
+        }
+
+    }
+    else{
+        alert('inavlid account number')
+    }
 }
